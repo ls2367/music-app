3f8e1081cbc7057607fc0e521d372144f6c6ac25
ls2367 <89614743+ls2367@users.noreply.github.com>
2021-12-04 16:22:02 -0500

    Added it
    again manually

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Lisa Sternik on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
